2b04a3cefcb0fe099542a5899d12b2096fba835c
Gabriel Henrique <gabrielhsmendonca@gmail.com>
2026-02-19 18:55:06 -0300

    chore: initial project setup

---
+++ src/test/java/com/gabrielmkv/file_converter/FileConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielmkv.file_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
