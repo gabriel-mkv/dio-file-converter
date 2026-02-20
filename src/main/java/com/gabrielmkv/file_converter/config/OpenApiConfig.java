c462612ce1f81cbfb77e536c2d5cfac3fbf0fb7c
Gabriel Henrique <gabrielhsmendonca@gmail.com>
2026-02-20 18:07:36 -0300

    chore: configure H2 database, data initialization, and Swagger paths

--- src/main/java/com/gabrielmkv/file_converter/config/OpenApiConfig.java
+++ src/main/java/com/gabrielmkv/file_converter/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.gabrielmkv.file_converter;
+package com.gabrielmkv.file_converter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
